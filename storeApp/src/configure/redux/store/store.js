949e98fde300245486c03163112b5c3aae1910ad
farrukhali7 <asyedfarrukh7@gmail.com>
2025-04-19 12:19:32 -0700

    Add files via upload

---
+++ storeApp/src/configure/redux/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '../reducers/cartSlice';
+import fetchReducer from '../reducers/fetchSlice';
+
+const store = configureStore({
+  reducer: {
+    fetch: fetchReducer, 
+    cart : cartReducer
+  },
+});
+
+export default store;
